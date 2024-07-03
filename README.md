2ee5ef15bfc688e5c0ababa1e77e8d1a9f706bbd
mep <apsxh65@gmail.com>
2024-07-03 18:15:48 +0900

    Dockerfile 	ubuntu -> python:3.9-slim-buster .gitignore 	.DS_store

--- README.md
+++ README.md
@@ -4,13 +4,16 @@
 
 # Docker
 ```bash
-sudo docker pull ubuntu:20.04
-sudo docker build -t my-python-app .
-docker run -it --rm --name my-running-app my-python-app
+sudo docker pull python:3.9-slim-buster
+sudo docker build -t podochung .
+docker run --rm pdochung
 ```
 
 # Execution
 ```bash
 python3 main.py {user ID}
 python3 main.py tempuser
+
+docker run --rm pdochung tempuser
+docker run --rm pdochung -d naver.com tempuser
 ```
--- main.py
+++ main.py
@@ -57,8 +57,8 @@ def find_user():
             print('-h, -help : 도움말을 확인합니다.')
             print('-o, -output : 결과를 파일로 출력합니다.')
             print('-f, -fast : 오탐방지 기능을 끕니다.')
-            print('username1, username2, ... : 검색할 유저명을 입력합니다.')
             print('-d, -domain : 입력된 도메인의 정보와 검색할 유저명을 비교합니다.')
+            print('username1, username2, ... : 검색할 유저명을 입력합니다.')
             exit(0)
         elif user == '-output' or user == '-o':
             output = True
